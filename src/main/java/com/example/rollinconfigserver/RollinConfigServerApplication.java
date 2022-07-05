bf09d6d047f5e0b0c29d11502a6181f9ad63f770
jeen0202 <71309053+jeen0202@users.noreply.github.com>
2022-07-05 14:37:06 +0900

    config initialize

---
+++ src/main/java/com/example/rollinconfigserver/RollinConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rollinconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollinConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollinConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rollinconfigserver/RollinConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rollinconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollinConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
